3c9ed7d11912fcbba223684a94887f30ba7d8cd6
matthieu-27 <matthieu27@pm.me>
2026-03-24 15:08:04 +0100

    feat: add console component

--- src/main/java/fr/fms/tutorial/TutorialApplication.java
+++ src/main/java/fr/fms/tutorial/TutorialApplication.java
@@ -1,26 +1,17 @@
 package fr.fms.tutorial;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import fr.fms.tutorial.dao.ArticleRepository;
-import fr.fms.tutorial.dao.CategoryRepository;
-import fr.fms.tutorial.entities.Article;
-import fr.fms.tutorial.entities.Category;
+import fr.fms.tutorial.console.App;
 
 @SpringBootApplication
 public class TutorialApplication implements CommandLineRunner {
 
 	@Autowired
-	private CategoryRepository categoryRepository;
-
-	@Autowired
-	private ArticleRepository articleRepository;
+	private App appMgr;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TutorialApplication.class, args);
@@ -28,103 +19,7 @@ public class TutorialApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// #region
-		// categoryRepository.save(new Category("Smartphone"));
-		// articleRepository.save(new Article("S21", "Samsung", 800));
-		// for (Article article : articleRepository.findByBrand("Samsung")) {
-		// System.out.println(article);
-		// }
-		// Category smartphone = categoryRepository.save(new Category("Smartphone"));
-		// Category tablet = categoryRepository.save(new Category("Tablettes"));
-		// Category pc = categoryRepository.save(new Category("PC"));
-
-		// articleRepository.save(new Article("S10", "Samsung", 500, smartphone));
-		// articleRepository.save(new Article("S9", "Samsung", 500, smartphone));
-		// articleRepository.save(new Article("MI10", "Xiaomi", 500, smartphone));
-		// articleRepository.save(new Article("Galaxy Tab S", "Samsung", 500,tablet));
-		// articleRepository.save(new Article("Ipad", "Apple", 1200, tablet));
-		// articleRepository.save(new Article("R510", "Asus", 900, pc));
-		// #endregion
-
-		// #region
-		// trouver 1 moyen d'afficher un article
-		Optional<Article> article = articleRepository.findById(Long.valueOf(1));
-		article.ifPresent(val -> System.out.println(val));
-		// et tout les articles...
-		List<Article> articles = articleRepository.findAll();
-		printArticles(articles);
-		// #endregion
-
-		// #region
-		// trouver par telle ou telle marque/description
-		List<Article> articleContains = articleRepository.findByBrandContainsAndDescriptionContains("S", "Sam");
-		printArticles(articleContains);
-		// #endregion
-
-		// #region
-		// test de la fonction search
-		List<Article> searchArts = articleRepository.findByBrandContainsAndPriceGreaterThan("S10", 200);
-		printArticles(searchArts);
-		// #endregion
-
-		// #region
-		// test du deleteById
-		Long id = Long.valueOf(1);
-		try {
-			articleRepository.deleteById(id);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		articles = articleRepository.findAll();
-		// on refait le tour pour vérifier
-		printArticles(articles);
-		// #endregion
-
-		// #region
-		Long updateId = Long.valueOf(2);
-
-		try {
-			Optional<Article> a = articleRepository.findById(updateId);
-			a.ifPresent(val -> {
-				val.setBrand("MIMI");
-				articleRepository.save(val);
-			});
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-		// #endregion
-
-		// #region
-		List<Category> ascList = categoryRepository.findAllByOrderByNameAsc();
-		printCategories(ascList);
-		List<Category> dscList = categoryRepository.findAllByOrderByNameDesc();
-		printCategories(dscList);
-		// #endregion
-
-		// #region
-		List<Article> catArticles = articleRepository.findByCategoryId(Long.valueOf(2));
-		printArticles(catArticles);
-		// #endregion
-	}
-
-	private void printCategories(List<Category> categories) {
-		printLine();
-		for (Category c : categories) {
-			System.out.println(c);
-		}
-		printLine();
-	}
-
-	private void printArticles(List<Article> articles) {
-		printLine();
-		for (Article a : articles) {
-			System.out.println(a);
-		}
-		printLine();
-	}
-
-	private void printLine() {
-		System.out.println("------------------------------------------------------------");
+		appMgr.start();
 	}
 
 }
---
+++ src/main/java/fr/fms/tutorial/console/App.java
@@ -0,0 +1,165 @@
+package fr.fms.tutorial.console;
+
+import java.util.List;
+import java.util.Scanner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import fr.fms.tutorial.dao.ArticleRepository;
+import fr.fms.tutorial.dao.CategoryRepository;
+import fr.fms.tutorial.entities.Article;
+
+@Component
+public class App {
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public void start() {
+        boolean running = true;
+        while (running) {
+            printMenu();
+            String choice = scanner.nextLine();
+            switch (choice) {
+                case "1":
+                    displayAllArticles();
+                    break;
+                case "2":
+                    displayArticlesWithPagination();
+                    break;
+                case "3":
+                    addArticle();
+                    break;
+                case "4":
+                    displayArticle();
+                    break;
+                case "5":
+                    deleteArticle();
+                    break;
+                case "6":
+                    updateArticle();
+                    break;
+                case "7":
+                    addCategory();
+                    break;
+                case "8":
+                    displayCategory();
+                    break;
+                case "9":
+                    deleteCategory();
+                    break;
+                case "10":
+                    updateCategory();
+                    break;
+                case "11":
+                    displayArticlesByCategory();
+                    break;
+                case "12":
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Choix invalide.");
+            }
+        }
+    }
+
+    private void displayArticlesByCategory() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayArticlesByCategory'");
+    }
+
+    private void updateCategory() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateCategory'");
+    }
+
+    private void deleteCategory() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteCategory'");
+    }
+
+    private void displayCategory() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayCategory'");
+    }
+
+    private void addCategory() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addCategory'");
+    }
+
+    private void updateArticle() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateArticle'");
+    }
+
+    private void deleteArticle() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteArticle'");
+    }
+
+    private void displayArticle() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayArticle'");
+    }
+
+    private void addArticle() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addArticle'");
+    }
+
+    private void displayArticlesWithPagination() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayArticlesWithPagination'");
+    }
+
+    private void printMenu() {
+        System.out.println("\nBienvenu dans notre application de gestion d'articles !");
+        System.out.println("1: Afficher tous les articles sans pagination");
+        System.out.println("2: Afficher tous les articles avec pagination");
+        System.out.println("3: Ajouter un article");
+        System.out.println("4: Afficher un article");
+        System.out.println("5: Supprimer un article");
+        System.out.println("6: Modifier un article");
+        System.out.println("7: Ajouter une categorie");
+        System.out.println("8: Afficher une categorie");
+        System.out.println("9: Supprimer une categorie");
+        System.out.println("10: Mettre a jour une categorie");
+        System.out.println("11: Afficher tous les articles d'une categorie");
+        System.out.println("12: Sortir du programme");
+        System.out.print("Votre choix: ");
+    }
+
+    private void displayAllArticles() {
+        List<Article> articles = articleRepository.findAll();
+        printArticles(articles);
+    }
+
+    private void printArticles(List<Article> articles) {
+        if (articles.isEmpty()) {
+            System.out.println("Aucun article trouvé.");
+            return;
+        }
+
+        System.out.println("\n");
+        System.out.printf("%-10s %-20s %-20s %-10s %-20s\n", "ID", "DESCRIPTION", "MARQUE", "PRIX", "CATEGORIE");
+        System.out.println(
+                "----------------------------------------------------------------------------------------------------");
+        for (Article article : articles) {
+            String categoryName = article.getCategory() != null ? article.getCategory().getName() : "Aucune";
+            System.out.printf("%-10d %-20s %-20s %-10.2f %-20s\n",
+                    article.getId(),
+                    article.getDescription(),
+                    article.getBrand(),
+                    article.getPrice(),
+                    categoryName);
+        }
+        System.out.println(
+                "----------------------------------------------------------------------------------------------------");
+    }
+}
